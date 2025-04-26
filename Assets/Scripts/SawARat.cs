decc1a3310eb9ee6535f432a0d60261c240ed00a
Metehan <gliencherth@gmail.com>
2025-04-26 10:42:35 +0300

    rat online

--- Assets/Scripts/SawARat.cs
+++ Assets/Scripts/SawARat.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class SawARat : MonoBehaviour
@@ -10,23 +11,28 @@ public class SawARat : MonoBehaviour
     // this script is attached to the player(a fox)
     
     public float radius = 5f; // the radius of the circle area
-    public LayerMask layerMask; // the layer mask to check for objects
     public float chaseSpeed = 5f; // the speed of the player when chasing the rat
     public float jumpForce = 10f; // the force of the jump
+    public bool grounded = false; // if the player is on the ground
+    
     
     private bool isChasing = false; // if the player is chasing the rat
     private GameObject rat; // the rat gameobject
     private GencTilkiController playerController; // the player controller script
     private Rigidbody2D rb; // the rigidbody of the player
+   
     
     
     void Start()
     {
         playerController = GetComponent<GencTilkiController>(); // get the player controller script
+        rb = GetComponent<Rigidbody2D>(); // get the rigidbody of the player
     }
     
     void Update()
     {
+        grounded= Physics2D.Raycast(transform.position, Vector2.down, 1.1f, LayerMask.GetMask("Ground")); // check if the player is on the ground
+        
         if (isChasing)
         {
             // check if the rat is still in the scene.
@@ -36,30 +42,33 @@ public class SawARat : MonoBehaviour
                 playerController.enabled = true; // enable the player controller
                 return;
             }
-
-            // check if rat is on the right or on the left.
-            Vector2 direction = ((rat.transform.position - transform.position) * Vector2.right).normalized; // get the direction to the rat
             
-            //check if the fox is near a cliff
-            Physics.Raycast(transform.position, new Vector2(direction.x*2, -1), out RaycastHit hit, 3f);
+            //rotate the player to the direction of the rat on y axis
+            Vector3 direction = (rat.transform.position - transform.position).normalized; // get the direction to the rat
+            if (direction.x > 0)
             {
-                if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Ground"))
-                {
-                    //standard chase
-                    rb.velocity = new Vector2(direction.x * chaseSpeed, rb.velocity.y); // move the player towards the rat
-                }
-                else
-                {
-                    //jump to the direction
-                    rb.AddForce(new Vector2(jumpForce*direction.x, jumpForce), ForceMode2D.Impulse); // jump to the right
-                }
+                transform.rotation=Quaternion.Euler(0,0,0); // set the player to face the rat
             }
+            else if (direction.x < 0)
+            {
+                transform.rotation=Quaternion.Euler(0,180,0); // set the player to face the rat
+            }
+            // Check for a cliff
+            bool isNearCliff = !Physics2D.Raycast(transform.position + new Vector3(transform.localScale.x * 0.5f, 0, 0), Vector2.down, 1.1f, LayerMask.GetMask("Ground"));
+
+            if (isNearCliff && grounded)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce); // make the fox jump
+            }
+            
+            rb.velocity = new Vector2(chaseSpeed * direction.x, rb.velocity.y); // move the player towards the rat
+            
         }
         else
         {
-            Collider[] colliders = Physics.OverlapSphere(transform.position, radius, layerMask); // check for objects in the circle area
+            Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, radius); // check for objects in the circle area
 
-            foreach (Collider collider in colliders)
+            foreach (Collider2D collider in colliders)
             {
                 if (collider.CompareTag("Fare")) // check if the object has a "Fare" tag
                 {
@@ -70,6 +79,7 @@ public class SawARat : MonoBehaviour
                 }
             }
         }
+        
     }
 
     private void OnDrawGizmos()
@@ -78,7 +88,7 @@ public class SawARat : MonoBehaviour
         Gizmos.DrawWireSphere(transform.position, radius); // draw a wire sphere around the player
      
         Gizmos.color = Color.green; // set the color of the gizmos
-        Gizmos.DrawLine(transform.position,transform.position+new Vector3(-2,-1,0)*3);
-        Gizmos.DrawLine(transform.position,transform.position+new Vector3(2,-1,0)*3);
+        Gizmos.DrawLine(transform.position,transform.position+new Vector3(-2,-1,0).normalized*5);
+        Gizmos.DrawLine(transform.position,transform.position+new Vector3(2,-1,0).normalized*5);
     }
 }
